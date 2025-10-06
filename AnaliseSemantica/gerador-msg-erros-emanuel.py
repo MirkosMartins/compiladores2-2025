a199fe9d39fec6cd0620e3b8e1c1017a011e029c
Emanuel Bissacotti <98053136+Emanuel-Bissacotti@users.noreply.github.com>
2025-10-06 10:37:41 -0300

    Add files via upload

---
+++ AnaliseSemantica/gerador-msg-erros-emanuel.py
@@ -0,0 +1,17 @@
+codigo = input("Digite o codigo: ")
+
+if codigo.find("++") != -1:
+    print(f"A variavel {codigo[:-2]} não é do tipo inteiro")
+else:
+    split = codigo.split("=")
+    try:
+        numero = int(codigo[:-1])
+        print(f"A variavel do tipo {split[0]} não é do tipo inteiro")
+    except:
+        if split[1].find("(") != -1 and split[1].find(")") != -1:
+            nome_funcao = split[1].split("(")[0]
+            print(f"A função {nome_funcao} espera um parametro do tipo '__' e foi passado do tipo '___'.")
+            print(f"O retorno da função {nome_funcao} não é do mesmo tipo de {split[0]}")
+        else:
+            print(f"A variavel {split[0]} não é do mesmo tipo de {split[1]}")
+                  
